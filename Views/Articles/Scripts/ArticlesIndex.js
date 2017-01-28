00ef28b3edc5764a00dca10e1165c3b4c29602ed
Kuba <bluttrommel@gmail.com>
2017-01-28 21:56:36 +0100

    Many layout fixes - flyingWindow, other (responsive too)

--- Views/Articles/Scripts/ArticlesIndex.js
+++ Views/Articles/Scripts/ArticlesIndex.js
@@ -41,18 +41,30 @@
         });
 
     $('.indexSingleArticleContainerDiary')
+        .mousemove(function (e) {
+            var flyingWindow = $(this).find('.flyingWindow');
+            flyingWindow.scrollTop();
+            flyingWindow.scrollLeft();
+            flyingWindow.css({
+                left: e.clientX + 15,
+                top: e.clientY + 15
+            });
+            if (flyingWindow.style.bottom <= 0) {
+                flyingWindow.css('bottom', 0);
+            }
+        })
         .mouseover(function () {
-            $(this).find('.imageOverlayColorDiary ').addClass('imageOverlayColorIndexNoOverlay', 400);
-            $(this).find('.indexDiaryName').addClass('indexSingleDiaryAnchorFocus', 300);
-            $(this).find('.indexDiaryName').animate({
-                right: $(this).find(".indexDiaryName").parent().width() / 2 - $(this).find(".indexDiaryName").width() / 2
-            }, 100);
+            //$(this).find('.imageOverlayColorDiary ').addClass('imageOverlayColorIndexNoOverlay', 400);
+            //$(this).find('.indexDiaryName').addClass('indexSingleDiaryAnchorFocus', 300);
+            //$(this).find('.indexDiaryName').animate({
+            //    right: $(this).find(".indexDiaryName").parent().width() / 2 - $(this).find(".indexDiaryName").width() / 2
+            //}, 100);
         })
         .mouseleave(function () {
-            $(this).find('.imageOverlayColorDiary ').removeClass('imageOverlayColorIndexNoOverlay', 0);
-            $(this).find('.indexDiaryName').removeClass('indexSingleDiaryAnchorFocus', 50);
-            $(this).find('.indexDiaryName').animate({
-                right: 0
-            }, 100);
+            //$(this).find('.imageOverlayColorDiary ').removeClass('imageOverlayColorIndexNoOverlay', 0);
+            //$(this).find('.indexDiaryName').removeClass('indexSingleDiaryAnchorFocus', 50);
+            //$(this).find('.indexDiaryName').animate({
+            //    right: 0
+            //}, 100);
         });
 });
