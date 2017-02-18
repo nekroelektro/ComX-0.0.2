8d6427b80f19ad8a54aafecba57754fd2e813964
Kuba <bluttrommel@gmail.com>
2017-02-18 10:49:08 +0100

    CkEditor replaced tinymce on comment, bundle.config

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Optimization;
+
+namespace ComX_0._0._2 {
+    public class BundleConfig {
+        public static void RegisterBundles(BundleCollection bundles) {
+            
+        }
+    }
+}
---
+++ scripts/config.js
@@ -0,0 +1,21 @@
+CKEDITOR.editorConfig = function (config) {
+    config.toolbarGroups = [
+		{ name: 'basicstyles', groups: ['basicstyles', 'cleanup'] },
+		{ name: 'clipboard', groups: ['clipboard', 'undo'] },
+		{ name: 'editing', groups: ['find', 'selection', 'spellchecker', 'editing'] },
+		{ name: 'links', groups: ['links'] },
+		{ name: 'insert', groups: ['insert'] },
+		{ name: 'forms', groups: ['forms'] },
+		{ name: 'tools', groups: ['tools'] },
+		{ name: 'document', groups: ['mode', 'document', 'doctools'] },
+		{ name: 'others', groups: ['others'] },
+		{ name: 'paragraph', groups: ['list', 'indent', 'blocks', 'align', 'bidi', 'paragraph'] },
+		{ name: 'styles', groups: ['styles'] },
+		{ name: 'colors', groups: ['colors'] },
+		{ name: 'about', groups: ['about'] }
+    ];
+
+    config.height = '8em';
+
+    config.removeButtons = 'Subscript,Superscript,Cut,Copy,Paste,PasteText,PasteFromWord,Scayt,Unlink,Anchor,Table,SpecialChar,Source,Blockquote,Outdent,NumberedList,BulletedList,Indent,About';
+};
