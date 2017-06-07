a32752f2e5cbbce021d8a16c6b0065ed901fb119
Kuba <bluttrommel@gmail.com>
2017-06-07 19:11:20 +0200

    before win reinstall

--- Views/Articles/Models/DtoModels/TopLogoDto.cs
+++ Views/Articles/Models/DtoModels/TopLogoDto.cs
@@ -4,5 +4,6 @@ namespace ComX_0._0._2.Views.Articles.Models.DtoModels {
     public class TopLogoDto {
         public Guid UserId { get; set; }
         public int MessagesCount { get; set; }
+        public string UserName { get; set; }
     }
 }
--- Views/Shared/Scripts/SharedTopLogoPanel.js
+++ Views/Shared/Scripts/SharedTopLogoPanel.js
@@ -1,84 +1,84 @@
 ﻿jQuery(document).ready(function($) {
-    var $loading = $('#loader').hide();
+    //var $loading = $('#loader').hide();
 
-    var searchIcon = $('.logoSearch');
-    var $container = $('.logoSection');
+    //var searchIcon = $('.logoSearch');
+    //var $container = $('.logoSection');
 
-    // SEARCH FUNCTIONALITY
-    NekroSearch($container, searchIcon);
+    //// SEARCH FUNCTIONALITY
+    //NekroSearch($container, searchIcon);
 
-    $('.popupLoginModal, .popupRegisterModal').magnificPopup({
-        type: 'inline',
-        preloader: false,
-        modal: true,
-        closeBtnInside: true,
-        showCloseBtn: true
-    });
+    //$('.popupLoginModal, .popupRegisterModal').magnificPopup({
+    //    type: 'inline',
+    //    preloader: false,
+    //    modal: true,
+    //    closeBtnInside: true,
+    //    showCloseBtn: true
+    //});
 
-    $('.popupLoginModal').click(function () {
-        $.ajax({
-            url: "/Account/Login/",
-            method: 'GET',
-            success: function (data) {
-                $('.loginContentInModal').html(data);
-            }
-        });
-    });
+    //$('.popupLoginModal').click(function () {
+    //    $.ajax({
+    //        url: "/Account/Login/",
+    //        method: 'GET',
+    //        success: function (data) {
+    //            $('.loginContentInModal').html(data);
+    //        }
+    //    });
+    //});
 
-    $('.popupRegisterModal').click(function () {
-        $.ajax({
-            url: "/Account/Register/",
-            method: 'GET',
-            success: function (data) {
-                $('.loginContentInModal').html(data);
-            }
-        });
-    });
+    //$('.popupRegisterModal').click(function () {
+    //    $.ajax({
+    //        url: "/Account/Register/",
+    //        method: 'GET',
+    //        success: function (data) {
+    //            $('.loginContentInModal').html(data);
+    //        }
+    //    });
+    //});
 
-    $(document).on('click', '.popupClose', function (e) {
-        e.preventDefault();
-        $.magnificPopup.close();
-    });
+    //$(document).on('click', '.popupClose', function (e) {
+    //    e.preventDefault();
+    //    $.magnificPopup.close();
+    //});
 
-    $('#logoImage').on("click",
-        function() {
-            window.location.href = "/";
-        });
+    //$('#logoImage').on("click",
+    //    function() {
+    //        window.location.href = "/";
+    //    });
 
-    //$('.logoLogoutAnchor').attr('href', 'javascript:document.getElementById("logoutForm").submit()');
-    var token = $('[name=__RequestVerificationToken]').val();
-    $('.logoLogoutAnchor').on("click",
-        function() {
-            $.ajax({
-                url: "/Account/LogOff",
-                method: "POST",
-                data: { '__RequestVerificationToken': token },
-                success: function() {
-                    location.reload(true);
-                }
-            });
-        });
+    ////$('.logoLogoutAnchor').attr('href', 'javascript:document.getElementById("logoutForm").submit()');
+    //var token = $('[name=__RequestVerificationToken]').val();
+    //$('.logoLogoutAnchor').on("click",
+    //    function() {
+    //        $.ajax({
+    //            url: "/Account/LogOff",
+    //            method: "POST",
+    //            data: { '__RequestVerificationToken': token },
+    //            success: function() {
+    //                location.reload(true);
+    //            }
+    //        });
+    //    });
     
-    $(document)
-      .ajaxStart(function () {
-          $loading.show();
-      })
-      .ajaxStop(function () {
-          $loading.hide();
-      });
+    //$(document)
+    //  .ajaxStart(function () {
+    //      $loading.show();
+    //  })
+    //  .ajaxStop(function () {
+    //      $loading.hide();
+    //  });
 
-    $('.logoProfileImageComponentAnchor').on("click",
-        function () {
-            if (!$('.logoComponentProfileContextMenu').is(":visible")) {
-                $('.logoComponentProfileContextMenu').slideDown('slow');
-            } else {
-                $('.logoComponentProfileContextMenu').slideUp('slow');
-            }
-        });
+    //$('.logoProfileImageComponentAnchor').on("click",
+    //    function () {
+    //        if (!$('.logoComponentProfileContextMenu').is(":visible")) {
+    //            $('.logoComponentProfileContextMenu').slideDown('slow');
+    //        } else {
+    //            $('.logoComponentProfileContextMenu').slideUp('slow');
+    //        }
+    //    });
 
-    $(document).mouseup(function(e) {
-        if ($('.logoComponentProfileContextMenu').is(":visible")) {
-            $('.logoComponentProfileContextMenu').slideUp('slow');
-        }
-    });
+    //$(document).mouseup(function(e) {
+    //    if ($('.logoComponentProfileContextMenu').is(":visible")) {
+    //        $('.logoComponentProfileContextMenu').slideUp('slow');
+    //    }
+    //});
 });
